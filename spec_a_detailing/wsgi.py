98096a39d5172821f169c23592c771ed0e3fc81e
Rhoshan <86419744+Gh0st102@users.noreply.github.com>
2025-02-13 02:52:32 +0000

    feat(all auth setup, allauth templates, blocks to base template)

---
+++ spec_a_detailing/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for spec_a_detailing project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'spec_a_detailing.settings')
+
+application = get_wsgi_application()
