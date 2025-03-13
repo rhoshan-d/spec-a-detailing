ed7b3ea9247e192138ece079b8ad23b51f85e154
Rhoshan <rhoshan02@gmail.com>
2025-03-13 18:39:39 +0000

    added checkout views and templates

--- spec_a_detailing/settings.py
+++ spec_a_detailing/settings.py
@@ -46,6 +46,9 @@ INSTALLED_APPS = [
     'products',
     'shoppingbag',
     'checkout',
+
+    # Other apps
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -59,6 +62,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'spec_a_detailing.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -75,6 +80,10 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'shoppingbag.contexts.bag_contents',
+            ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
             ],
         },
     },
