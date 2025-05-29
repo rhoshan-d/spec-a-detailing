2ab91b49063b8ccd4bc50479828edb5de834c4c8
Rhoshan <86419744+Gh0st102@users.noreply.github.com>
2025-05-29 02:29:01 +0100

    Fix typo in MESSAGE_STORAGE setting in settings.py

--- spec_a_detailing/settings.py
+++ spec_a_detailing/settings.py
@@ -103,7 +103,7 @@ TEMPLATES = [
     },
 ]
 
-MESSAGESES_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
 AUTHENTICATION_BACKENDS = [
     # Needed to login by username in Django admin, regardless of `allauth`
